c058b189d0082b3dd286ba43f55ba9aefe5d31bb
Abhrankan Chakrabarti <abhrankan@gmail.com>
2024-06-15 18:42:49 +0530

    Add files via upload

--- js/coderunner.js
+++ js/coderunner.js
@@ -1,45 +1,58 @@
 async function loadSample() {
-    const selectedSample = document.getElementById("samplePrograms").value;
-    const codeInput = document.getElementById("codeInput");
+    const selectedSample = document.getElementById('samplePrograms').value;
+    const codeInput = document.getElementById('codeInput');
     if (selectedSample) {
         try {
             const response = await fetch(`samples/${selectedSample}.js`);
             if (!response.ok) {
-                throw new Error("Network response was not ok");
+                throw new Error('Network response was not ok');
             }
             const sampleCode = await response.text();
             codeInput.value = sampleCode;
         } catch (error) {
-            console.error("Error fetching the sample code:", error);
-            codeInput.value = "Error loading sample code.";
+            console.error('Error fetching the sample code:', error);
+            codeInput.value = 'Error loading sample code.';
         }
     }
 }
 
 function runCode() {
-    const code = document.getElementById("codeInput").value;
-    const outputElement = document.getElementById("output");
-    outputElement.textContent = ""; // Clear previous output
-
+    const code = document.getElementById('codeInput').value;
+    const outputElement = document.getElementById('output');
+    outputElement.textContent = ''; // Clear previous output
+    
     // Save the original console.log
     const originalConsoleLog = console.log;
 
     // Override console.log
     console.log = (...args) => {
         // Convert all arguments to strings and concatenate them
-        const output = args.map((arg) => (typeof arg === "object" ? JSON.stringify(arg) : String(arg))).join(" ");
+        const output = args.map(arg => typeof arg === 'object' ? JSON.stringify(arg) : String(arg)).join(' ');
         // Display the output in the outputElement and also add a newline for each call
-        outputElement.textContent += output + "\n";
+        outputElement.textContent += output + '\n';
     };
 
     try {
         eval(code);
     } catch (e) {
-        outputElement.textContent = "Error: " + e.message;
+        outputElement.textContent = 'Error: ' + e.message;
     } finally {
         // Restore the original console.log
         console.log = originalConsoleLog;
     }
+}
+
+function save(id, attribute, file) {
+    const code = document.getElementById(id)[attribute];
+    const blob = new Blob([code], { type: 'text/javascript' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = file;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
 }
 
 function loadFile() {
